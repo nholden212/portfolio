3d4470471690b31e8f07e939fc2e44bde097bcdc
Nick Holden <nholden212@gmail.com>
2018-12-08 17:59:20 -0800

    Implemented basic image slideshow functionality within project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 
 class Project extends Component {
-  constructor(props) {
-      super(props);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            i: 0
+        }
+    }
+
+    slideToTheLeft = () => {
+        if(this.state.i === 0){
+            this.setState({ i: this.props.images.length - 1 });
+        } else {
+            this.setState({ i: this.state.i - 1 });
+        }
+    }
+
+    slideToTheRight = () => {
+        if(this.state.i === this.props.images.length - 1){
+            this.setState({ i: 0 })
+        } else {
+            this.setState({ i: this.state.i + 1});
+        }
+    }
   
     render() {
-    return (
-      <div className="Project">
-        <h3 className="ProjectTitle">{this.props.title}</h3>
-        <p className="ProjectDescription">{this.props.description}</p>
-        <div className="ProjectSlideshow">Screenshot slideshow will go here</div>
-        <h5 className="ProjectDeployedURL">Check out the deployed project <a className="" href={this.props.deployedURL}>here</a></h5>
-        <h5 className="ProjectGitHubURL">Check out the code on GitHub <a href={this.props.gitHubURL}>here</a></h5>
-      </div>
-    );
-  }
+        return (
+            <div className="Project">
+                <h3 className="ProjectTitle">{this.props.title}</h3>
+                <p className="ProjectDescription">{this.props.description}</p>
+                
+                <div className="ProjectSlideshow">
+                    <button onClick={this.slideToTheLeft}>Back</button>
+                    <button onClick={this.slideToTheRight}>Forward</button>
+                    <img 
+                        src={this.props.images[this.state.i].src}
+                        alt={this.props.images[this.state.i].alt}>
+                    </img>
+                </div>
+                
+                <h5 className="ProjectDeployedURL">
+                    Check out the deployed project <a className="" href={this.props.deployedURL}>here</a>
+                </h5>
+                
+                <h5 className="ProjectGitHubURL">
+                    Check out the code on GitHub <a href={this.props.gitHubURL}>here</a>
+                </h5>
+            </div>
+        );
+    }
 }
 
 export default Project;
--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import Project from './Project';
 
 class ProjectsPage extends Component {
+
   render() {
     return (
       <div className="AboutPage">
         <h1>Projects</h1>
-        <p>More content coming - stay tuned!</p>
-        <Project title="ExampleTitle" deployedURL="https://www.reddit.com/r/survivor/" />
+        <Project
+          title="Example Project"
+          description="A brief paragraph describing the project will go here."
+          images={['']}
+          deployedURL=''
+          gitHubURL=''
+        />
       </div>
     );
   }
