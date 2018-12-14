765adcbe4fbf57570c8b972c65884890deb747cf
Nick Holden <nholden212@gmail.com>
2018-12-13 17:04:21 -0800

    Implemented contact form submission via firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 
+import * as firebase from 'firebase';
+
 import AboutPage from './components/AboutPage';
 import ProjectsPage from './components/ProjectsPage';
 import ResumePage from './components/ResumePage';
@@ -9,6 +11,17 @@ import ContactPage from './components/ContactPage';
 import MusicPage from './components/MusicPage';
 
 import { FaUser, FaCode, FaFile, FaEnvelope, FaMusic } from 'react-icons/fa';
+
+var config = {
+  apiKey: "AIzaSyD3laknF4pjp0EESF-u4Dstly-_frk-Q1k",
+    authDomain: "portfolio-98c08.firebaseapp.com",
+    databaseURL: "https://portfolio-98c08.firebaseio.com",
+    projectId: "portfolio-98c08",
+    storageBucket: "portfolio-98c08.appspot.com",
+    messagingSenderId: "499786435163"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -27,7 +40,7 @@ class App extends Component {
           <Route path="/about" component={AboutPage} />
           <Route path="/projects" component={ProjectsPage} />
           <Route path="/resume" component={ResumePage} />
-          <Route path="/contact" component={ContactPage} />
+          <Route path="/contact" render={(props) => <ContactPage firebase={firebase} />} />
           <Route path="/music" component={MusicPage} />
         </main>
       </div>
--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 class ContactPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      message: ""
+    }
+    this.messagesRef = this.props.firebase.database().ref('contact-messages');
+  }
+  
 
   contactFormSubmit(e) {
-    console.log("contactFormSubmit called");
+    e.preventDefault();
+    if (!this.state.firstName ||
+        !this.state.lastName ||
+        !this.state.email ||
+        !this.state.message) { return }
+    this.messagesRef.push({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      message: this.state.message
+    });
+    this.setState({ firstName: "", lastName: "", email: "", message: "" });
+  }
+
+  updateFirstName(e) {
+    this.setState({ firstName: e.target.value });
+  }
+
+  updateLastName(e) {
+    this.setState({ lastName: e.target.value });
+  }
+
+  updateEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  updateMessage(e) {
+    this.setState({ message: e.target.value });
   }
 
   render() {
@@ -15,20 +53,20 @@ class ContactPage extends Component {
         <form className="ContactForm" onSubmit={(e) => this.contactFormSubmit(e)}>
           <h4>Contact me:</h4>
           
-            <label for="contact-firstName">First name:</label>
-            <input type="text" id="contact-firstName"></input>
+            <label hmtlFor="contact-firstName">First name:</label>
+            <input type="text" id="contact-firstName" value={this.state.firstName} onChange={(e) => this.updateFirstName(e)}></input>
             <br></br>
             
-            <label for="contact-lastName">Last name:</label>
-            <input type="text" id="contact-lastName"></input>
+            <label htmlFor="contact-lastName">Last name:</label>
+            <input type="text" id="contact-lastName" value={this.state.lastName} onChange={(e) => this.updateLastName(e)}></input>
             <br></br>
             
-            <label for="contact-email">Your email:</label>
-            <input type="email" id="contact-email"></input>
+            <label htmlFor="contact-email">Your email:</label>
+            <input type="email" id="contact-email" value={this.state.email} onChange={(e) => this.updateEmail(e)}></input>
             <br></br>
             
-            <label for="contact-body">Leave a message:</label>
-            <textarea id="contact-body"></textarea>
+            <label htmlFor="contact-body">Leave a message:</label>
+            <textarea id="contact-body" value={this.state.message} onChange={(e) => this.updateMessage(e)}></textarea>
             <br></br>
             
             <button type="Submit">Submit</button>
