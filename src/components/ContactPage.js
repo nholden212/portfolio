2532a65d922359d9fc0cabb7567db92deda8212f
Nick Holden <nholden212@gmail.com>
2018-12-13 16:16:44 -0800

    Email and contact form added to contact page

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 class ContactPage extends Component {
+
+  contactFormSubmit(e) {
+    console.log("contactFormSubmit called");
+  }
+
   render() {
     return (
       <div className="AboutPage">
         <h1>Contact</h1>
-        <p>More content coming - stay tuned!</p>
+        <p><strong><a href="mailto:nholden212@gmail.com" target="_blank">Email Me!</a></strong></p>
+        <p>If you don't have a default email set up, you can also get in touch with me by filling out the form below:</p>
+        <form className="ContactForm" onSubmit={(e) => this.contactFormSubmit(e)}>
+          <h4>Contact me:</h4>
+          
+            <label for="contact-firstName">First name:</label>
+            <input type="text" id="contact-firstName"></input>
+            <br></br>
+            
+            <label for="contact-lastName">Last name:</label>
+            <input type="text" id="contact-lastName"></input>
+            <br></br>
+            
+            <label for="contact-email">Your email:</label>
+            <input type="email" id="contact-email"></input>
+            <br></br>
+            
+            <label for="contact-body">Leave a message:</label>
+            <textarea id="contact-body"></textarea>
+            <br></br>
+            
+            <button type="Submit">Submit</button>
+
+        </form>
       </div>
     );
   }
